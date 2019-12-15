25f019e4bdefdbdf4257318326ff12c257b22977
Elijah Rosier BSc (Hons) MIET <Raptosaur@users.noreply.github.com>
2019-12-15 22:12:01 +0000

    fix typo in header

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ClubHabbo » Cooler then the Ice Cafe!</title>
+    <title>ClubHabbo » Cooler than the Ice Cafe!</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
