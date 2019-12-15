a0705704c08d9dfac974efe630c0ebabeb77e048
Joshua Blackman <joshuadennisblackman@gmail.com>
2019-12-15 17:13:22 +0000

    update title

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} >></title>
+    <title>ClubHabbo » Cooler then the Ice Cafe!</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,7 +25,7 @@
         <div class="header mt-2">
             <div class="row">
                 <div class="col-12">
-                    <img src="/img/logo.gif" class="logo" alt="Classic Habbox"/>
+                    <img src="/img/logo.gif" class="logo" alt="ClubHabbo"/>
                 </div>
 
                 <div class="col-12">
